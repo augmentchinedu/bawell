b8c41aa82b9392391b0021f39bcb7a97c9af7530
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-25 21:17:34 +0100

    Downloads CMD Folder

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(router);
 
 app.get("/", (req, res) => {
 	res.json({
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,31 @@
-module.exports = {};
+const express = require("express");
+const router = express.Router();
+
+const fs = require("fs");
+const archiver = require("archiver");
+const path = require("path");
+
+router.get("/download", async (req, res) => {
+	const folderToZip = path.join(__dirname, "..", "cmd");
+	const zipName = "bawell.zip";
+
+	res.setHeader("Content-Disposition", `attachment; filename=${zipName}`);
+	res.setHeader("Content-Type", "application/zip");
+
+	const archive = archiver("zip", { zlib: { level: 9 } });
+
+	// Stream archive data to the response
+	archive.pipe(res);
+
+	// Append entire folder
+	archive.directory(folderToZip, false);
+
+	archive.finalize();
+
+	archive.on("error", (err) => {
+		console.error("Archive error:", err);
+		res.status(500).send({ error: "Could not create zip" });
+	});
+});
+
+module.exports = router;
