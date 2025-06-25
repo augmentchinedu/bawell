2778ed8e8d792ba86d98ebc4b5d39b0851e59cef
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-26 00:47:54 +0100

    WS Added

--- cmd/index.js
+++ cmd/index.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 const { init } = require("./core");
+
+const WebSocket = require("ws");
+const http = require("http");
 const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
 const express = require("express");
 
 const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
 
 const router = require("./routes");
 
@@ -26,12 +32,29 @@ app.all("/{*any}", (req, res) => {
 	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
+// WebSocket logic
+wss.on("connection", (ws) => {
+	console.log("Client connected via WebSocket");
+
+	// Send current directory of this file
+	ws.send(
+		JSON.stringify({
+			type: "server-dir",
+			dir: __dirname,
+		})
+	);
+
+	ws.on("message", (msg) => {
+		console.log("Message from client:", msg);
+	});
+});
+
 const PORT = process.env.PORT || 2001;
 
 (async () => {
 	await init();
 
-	app.listen(PORT, () => {
+	server.listen(PORT, () => {
 		console.log(`Server Started @ ${PORT}`);
 	});
 })();
